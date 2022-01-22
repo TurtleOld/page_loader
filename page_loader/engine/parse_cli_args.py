5d02ff5bf4cdb01b41e0c2468a31096ec1073542
TurtleOld <shurpo.alexander@gmail.com>
2022-01-22 22:53:23 +0300

    rewrite arguments

--- page_loader/engine/parse_cli_args.py
+++ page_loader/engine/parse_cli_args.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 DESCRIPTION = 'Internet page loader'
 OUTPUT_FLAG_1 = '-o'
@@ -11,9 +12,10 @@ VERSION_NUMBER = f'{DESCRIPTION} 1.0'
 
 def parse_cli_arguments():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
-    parser.add_argument('<url>')
     parser.add_argument(OUTPUT_FLAG_1, OUTPUT_FLAG_2,
-                        help=OUTPUT_HELP, action='store_true')
+                        help=OUTPUT_HELP,
+                        default=os.getcwd())
+    parser.add_argument('url', type=str)
     parser.add_argument(VERSION_STRING_1, VERSION_STRING_2,
                         version=VERSION_NUMBER, action='version')
     args = parser.parse_args()
