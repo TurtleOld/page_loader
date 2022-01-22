15c972358fa8aacf951f7a27b3e6d693ad2a5a6c
TurtleOld <shurpo.alexander@gmail.com>
2022-01-22 22:54:38 +0300

    refactoring test func

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,20 +1,18 @@
-import os
 import tempfile
 
 import requests
 from requests_mock.mocker import Mocker
 
+from page_loader import download
+
 link = 'https://ru.hexlet.io/courses'
-new_link = link.replace('https://', '').replace('.', '-').replace('/', '-')
-file_name = f'{new_link}.html'
-html = requests.get(link)
+temp_dir = tempfile.TemporaryDirectory()
+file_name = download(link, temp_dir.name)
 
 
-def test_page_loader(requests_mock: Mocker):
-    temp_dir = tempfile.TemporaryDirectory()
-    with open(os.path.join(temp_dir.name, file_name), 'w') as file1:
-        file1.write(html.text)
-        with open(file1.name, 'r') as file2:
-            data = file2.read()
-            requests_mock.get(link, text=data)
-            assert data == requests.get(link).text
+def test_download(requests_mock: Mocker):
+    with open(file_name, 'r', encoding='utf-8') as file2:
+        data = file2.read()
+        requests_mock.get(link, text=data)
+
+        assert data == requests.get(link).text
