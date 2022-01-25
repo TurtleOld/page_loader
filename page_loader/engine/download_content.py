b150ad903a04d7cc9f294977bbb3eddc2be96b68
TurtleOld <shurpo.alexander@gmail.com>
2022-01-25 16:59:17 +0300

    refactor

--- page_loader/engine/download_content.py
+++ page_loader/engine/download_content.py
@@ -5,13 +5,13 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_link_without_protocol(url):
-    return re.sub(r'^(http|https)://', '', url)
+def get_new_link_format(url):
+    link_without_protocol = re.sub(r'^(http|https)://', '', url)
+    return re.sub(r'\W', '-', link_without_protocol)
 
 
 def download_images(url, path=os.getcwd()):
-    link_without_protocol = get_link_without_protocol(url)
-    string = re.sub(r'\W', '-', link_without_protocol)
+    string = get_new_link_format(url)
     folder_name = f'{string}_files'
     full_path = os.path.join(path, folder_name)
 
@@ -21,11 +21,9 @@ def download_images(url, path=os.getcwd()):
     page_content = requests.get(url)
     soup = BeautifulSoup(page_content.content, 'html.parser')
     for tag in soup.find_all('img'):
-        link_img_without_protocol = get_link_without_protocol(tag['src'])
         link_img = tag['src']
-        path_name = re.sub(r'\W', '-',
-                           os.path.dirname(link_img_without_protocol))
-        file_name = f'{path_name}-{os.path.basename(link_img_without_protocol)}'
+        path_name = get_new_link_format(os.path.dirname(tag['src']))
+        file_name = f'{path_name}-{os.path.basename(link_img)}'
         with open(os.path.join(path, folder_name, file_name), 'wb') as \
                 full_path_name:
             full_path_name.write(requests.get(f'{url}{link_img}').content)
