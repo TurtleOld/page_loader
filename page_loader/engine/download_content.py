c35e7d1527302f3495c4725a58ed716caf7e40ae
TurtleOld <shurpo.alexander@gmail.com>
2022-01-26 15:34:16 +0300

    added several func

--- page_loader/engine/download_content.py
+++ page_loader/engine/download_content.py
@@ -2,7 +2,9 @@ import os
 import re
 
 import requests
+import tldextract
 from bs4 import BeautifulSoup
+from bs4.element import Tag
 
 
 def get_new_link_format(url):
@@ -10,7 +12,20 @@ def get_new_link_format(url):
     return re.sub(r'\W', '-', link_without_protocol)
 
 
-def download_images(url, path=os.getcwd()):
+def get_content(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        print(response.status_code)
+
+
+def soup_parser(url):
+    html = get_content(url)
+    return BeautifulSoup(html, 'html.parser')
+
+
+def create_folder(url, path):
     string = get_new_link_format(url)
     folder_name = f'{string}_files'
     full_path = os.path.join(path, folder_name)
@@ -18,13 +33,43 @@ def download_images(url, path=os.getcwd()):
     if not os.path.isdir(full_path):
         os.mkdir(full_path)
 
-    page_content = requests.get(url)
-    soup = BeautifulSoup(page_content.content, 'html.parser')
-    for tag in soup.find_all('img'):
-        link_img = tag['src']
-        path_name = get_new_link_format(os.path.dirname(tag['src']))
-        file_name = f'{path_name}-{os.path.basename(link_img)}'
-        with open(os.path.join(path, folder_name, file_name), 'wb') as \
-                full_path_name:
-            full_path_name.write(requests.get(f'{url}{link_img}').content)
-            return os.path.join(folder_name, file_name)
+    return folder_name
+
+
+def get_domain_suite(url):
+    domain = tldextract.extract(url)
+    return f'{domain.subdomain}-{domain.domain}-{domain.suffix}'
+
+
+def parse_tags(url: str, tag: Tag):
+    tags_url = tag['src']
+    domain_name = get_domain_suite(url)
+    path_name = get_new_link_format(os.path.dirname(tags_url))
+    file_name = f'{domain_name}-{path_name}-{os.path.basename(tags_url)}'
+    return tags_url, file_name
+
+
+def prepare(url):
+    soup = soup_parser(url)
+    tags = soup.find_all('img')
+    result = []
+    for tag in tags:
+        links = parse_tags(url, tag)
+        result.append(links)
+    return result
+
+
+def download_images(url, path):
+    folder_name = create_folder(url, path)
+    links = prepare(url)
+    result = []
+    for link in links:
+        if link[0].startswith('/'):
+            with open(os.path.join(path, folder_name, link[1]), 'wb') as file_name:
+                file_name.write(requests.get(f'{url}{link[0]}').content)
+                result.append((folder_name, link[1]))
+        else:
+            with open(os.path.join(path, folder_name, link[1]), 'wb') as file_name:
+                file_name.write(requests.get(link[0]).content)
+                result.append((folder_name, link[1]))
+    return result
