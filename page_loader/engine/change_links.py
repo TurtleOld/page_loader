51847024148da325a4407a56205b3cfd0721abe9
TurtleOld <shurpo.alexander@gmail.com>
2022-01-26 15:33:45 +0300

    refactor

--- page_loader/engine/change_links.py
+++ page_loader/engine/change_links.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 def change_links(name_file, links):
     with open(name_file.name, 'r', encoding='utf-8') as file_path:
         soup = BeautifulSoup(file_path, 'html.parser')
-        for tag in soup.find_all('img'):
+        tags = soup.find_all('img')
+        for tag in tags:
             tag['src'] = links
         with open(name_file.name, 'w', encoding='utf-8') as new_file_path:
-            new_file_path.write(str(soup))
+            new_file_path.write(str(soup.prettify(formatter='minimal')))
--- page_loader/engine/download.py
+++ page_loader/engine/download.py
@@ -1,17 +1,16 @@
 import os
-import re
 
 import requests
 
 from page_loader.engine.change_links import change_links
-from page_loader.engine.download_content import download_images
+from page_loader.engine.download_content import download_images, \
+    get_new_link_format
 
 CURRENT_DIRECTORY = os.getcwd()
 
 
 def download(url, path=CURRENT_DIRECTORY):
-    without_protocol = url.replace('https://', '')
-    new_link = re.sub(r'\W', '-', without_protocol)
+    new_link = get_new_link_format(url)
     file_name = f'{new_link}.html'
     request_link = requests.get(url)
     with open(os.path.join(path, file_name), 'w',
