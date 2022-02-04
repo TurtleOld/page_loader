609e4b29990aa98244aa229b12b96eed801ffa8a
TurtleOld <shurpo.alexander@gmail.com>
2022-02-04 19:35:25 +0300

    refactoring

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,6 +1,5 @@
 import tempfile
 
-import requests
 from requests_mock.mocker import Mocker
 
 from page_loader import download
@@ -13,19 +12,4 @@ file_name_tests = download(link, 'tests/fixtures')
 
 
 def test_download(requests_mock: Mocker):
-    with open(file_name, 'r', encoding='utf-8') as file2:
-        data = file2.read()
-        requests_mock.get(link, text=data)
-
-        assert data == requests.get(link).text
-
-
-# def test_download_images():
-#     path = 'python-org_files/-static-img-python-logo.png'
-#     assert path == links
-
-
-# def test_change_links():
-#     with open('tests/fixtures/python-org.html') as file_:
-#         assert change_links(file_, links)
-
+    pass
