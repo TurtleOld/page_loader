113f005c33e2321d2581b4fd5868656d4aecc091
TurtleOld <shurpo.alexander@gmail.com>
2022-02-07 13:34:46 +0300

    added checks for tests

--- page_loader/engine/download.py
+++ page_loader/engine/download.py
@@ -1,12 +1,12 @@
 import os
 
-from page_loader.engine.download_content import get_content, download_images
+from page_loader.engine.download_content import get_content, download_content
 
 CURRENT_DIRECTORY = os.getcwd()
 
 
 def download(url, path=CURRENT_DIRECTORY):
     file_with_content = get_content(url, path)
-    download_images(url, path)
+    download_content(url, path)
 
     return file_with_content
--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -9,7 +9,11 @@ from page_loader.engine.download_content import create_folder, get_content
 
 URL = 'https://page-loader.hexlet.repl.co'
 internet_path_image = 'assets/professions/nodejs.png'
+internet_path_css = 'assets/application.css'
+internet_path_js = 'script.js'
 url_image = os.path.join(URL, internet_path_image)
+url_css = os.path.join(URL, internet_path_css)
+url_js = os.path.join(URL, internet_path_js)
 
 path_original = os.path.join('tests', 'fixtures', 'downloads')
 downloads_dir = os.path.join('tests', 'fixtures', 'downloads', 'changed')
@@ -18,11 +22,18 @@ html_file_name = os.path.join(path_original, 'page-loader-hexlet-repl-co.html')
 changed_html_file_name = 'page-loader-hexlet-repl-co.html'
 created_dir_name = 'page-loader-hexlet-repl-co_files'
 image_name = 'page-loader-hexlet-repl-co--assets-professions-nodejs.png'
+css_name = 'page-loader-hexlet-repl-co--assets-application.css'
+js_name = 'page-loader-hexlet-repl-co--script.js'
+
 
 created_html_file = os.path.join(downloads_dir, changed_html_file_name)
 created_image = os.path.join(created_dir_name, image_name)
+created_css = os.path.join(created_dir_name, css_name)
+created_js = os.path.join(created_dir_name, js_name)
 
 expected_image = os.path.join(downloads_dir, created_image)
+expected_css = os.path.join(downloads_dir, created_css)
+expected_js = os.path.join(downloads_dir, created_js)
 
 
 def read_file(file):
@@ -38,12 +49,16 @@ def test_folder_creation():
 
 @pytest.mark.parametrize('expected', [
     changed_html_file_name,
-    created_image
+    created_image,
+    created_css,
+    created_js
 ])
 def test_download_content(expected):
     with requests_mock.Mocker(real_http=True) as mock:
         mock.get(URL, content=read_file(created_html_file))
         mock.get(url_image, content=read_file(expected_image))
+        mock.get(url_css, content=read_file(expected_css))
+        mock.get(url_js, content=read_file(expected_js))
         with tempfile.TemporaryDirectory() as directory:
             download(URL, directory)
             expected_path = os.path.join(directory, expected)
