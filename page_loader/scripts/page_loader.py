9e02cbba8abb221f113954681b7675b3accc8c70
TurtleOld <shurpo.alexander@gmail.com>
2022-02-08 22:17:26 +0300

    added handler logging

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,12 +1,25 @@
 # ! /usr/bin/env python
+
+import logging.config
+import sys
+
 from page_loader.engine.download import download
 from page_loader.engine.parse_cli_args import parse_cli_arguments
+
+log = logging.getLogger(__name__)
+
+LOG_FORMAT = '%(message)s'
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO, format=LOG_FORMAT)
 
 
 def main():
     args = parse_cli_arguments()
-    file_path = download(args.url, args.output)
-    print(file_path)
+    try:
+        file_path = download(args.url, args.output)
+        log.info(f"Page was successfully downloaded into '{file_path}'")
+    except FileNotFoundError:
+        log.error(f'ERROR: No such directory: {args.output}')
 
 
 if __name__ == '__main__':
