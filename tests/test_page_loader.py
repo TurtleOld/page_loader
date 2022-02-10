69d08e7e9966ddbb7a3f4c5b9f65fe6cacad95c6
TurtleOld <shurpo.alexander@gmail.com>
2022-02-10 12:33:40 +0300

    refactoring

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -6,8 +6,7 @@ import requests
 from requests_mock import Mocker
 
 from page_loader import download
-from page_loader.engine.download_content import create_folder, get_html_file, \
-    get_content
+from page_loader.engine.download_content import create_folder, get_html_file
 
 URL = 'https://page-loader.hexlet.repl.co'
 INVALID_URL = 'https://badsite.com'
@@ -96,10 +95,3 @@ def test_connection_error(requests_mock: Mocker):
             assert download(INVALID_URL, tmp_dir_name)
 
         assert not os.listdir(tmp_dir_name)
-
-
-def test_get_content():
-    try:
-        get_content(URL)
-    except Exception as exc:
-        assert pytest.fail(exc, pytrace=True)
