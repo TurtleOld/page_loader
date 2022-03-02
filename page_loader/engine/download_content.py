a2902ac0f36db9ecefb80ae2aafe698757eb18ba
TurtleOld <shurpo.alexander@gmail.com>
2022-03-02 22:23:05 +0300

    refactoring

--- page_loader/engine/download_content.py
+++ page_loader/engine/download_content.py
@@ -179,20 +179,21 @@ def download_content(url, path):
                              get_content(f'{urls}{tag["src"]}'))
         if tag['src'].startswith('http') and '.' in tag['src']:
             file_name = f'{os.path.basename(tag["src"])}'
-            print('file_name', file_name)
-            print('file_extension', os.path.isfile(file_name))
-            paths = os.path.dirname(tag['src'])
-            print('paths', paths)
-            print(tag["src"])
-            print('save', os.path.join(path, folder_name,
-                                       f'{domain_name}'
-                                       f'{get_new_link_format(paths)}-'
-                                       f'{file_name}'))
-            save_to_file(os.path.join(path, folder_name,
-                                      f'{domain_name}'
-                                      f'{get_new_link_format(paths)}-'
-                                      f'{file_name}'),
-                         get_content(tag["src"]))
+            if os.path.isfile(file_name):
+                print('file_name', file_name)
+                print('file_extension', os.path.isfile(file_name))
+                paths = os.path.dirname(tag['src'])
+                print('paths', paths)
+                print(tag["src"])
+                print('save', os.path.join(path, folder_name,
+                                           f'{domain_name}'
+                                           f'{get_new_link_format(paths)}-'
+                                           f'{file_name}'))
+                save_to_file(os.path.join(path, folder_name,
+                                          f'{domain_name}'
+                                          f'{get_new_link_format(paths)}-'
+                                          f'{file_name}'),
+                             get_content(tag["src"]))
 
     for tag_ in tags_href:
 
