3d2dfa26de9bc6d90e216006740dff0da3d382c4
TurtleOld <shurpo.alexander@gmail.com>
2022-03-19 17:26:45 +0300

    added logging

--- page_loader/engine/save_file.py
+++ page_loader/engine/save_file.py
@@ -1,3 +1,8 @@
+import os
+
+from page_loader.engine.logger_config import logger
+
+
 def save_to_file(path_to_file, data):
     """
     A function that allows you to save files in bytes and regular.
@@ -6,8 +11,12 @@ def save_to_file(path_to_file, data):
     """
     if isinstance(data, bytes):
         with open(path_to_file, 'wb') as file_name:
+            logger.info(f'Link {os.path.basename(file_name.name)} is downloaded'
+                        )
             file_name.write(data)
 
     else:
         with open(path_to_file, 'w', encoding='utf-8') as file_name:
+            logger.info(f'Link {os.path.basename(file_name.name)} is downloaded'
+                        )
             file_name.write(data)
