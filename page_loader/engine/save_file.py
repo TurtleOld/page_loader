9fc6cc541c89c3f4df532f358b1c8ed859a644a8
TurtleOld <shurpo.alexander@gmail.com>
2022-03-23 22:20:59 +0300

    Comments on item 22

--- page_loader/engine/save_file.py
+++ page_loader/engine/save_file.py
@@ -15,14 +15,3 @@ def save_to_file(path_to_file, data):
     with open(path_to_file, format_file) as file_name:
         file_name.write(data)
     logger.info(f'Link {os.path.basename(file_name.name)} is downloaded')
-    # if isinstance(data, bytes):
-    #     with open(path_to_file, 'wb') as file_name:
-    #         logger.info(f'Link {os.path.basename(file_name.name)} is '
-    #                     f'downloaded')
-    #         file_name.write(data)
-    #
-    # else:
-    #     with open(path_to_file, 'w', encoding='utf-8') as file_name:
-    #         logger.info(f'Link {os.path.basename(file_name.name)} is '
-    #                     f'downloaded')
-    #         file_name.write(data)
