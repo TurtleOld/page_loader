da3254762f261799431e97f8f0fd761c1c3678c0
TurtleOld <shurpo.alexander@gmail.com>
2022-04-01 00:05:08 +0300

    refactoring after Mentor's comment

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -19,21 +19,22 @@ def main():
               f'{error.filename}')
         logger_error.error(f'Permission denied to the specified directory: '
                            f'{error.filename}')
-
+        sys.exit(1)
     except FileNotFoundError as error2:
         print(f'The system cannot find the path specified: '
               f'{error2.filename}')
         logger_error.error(f'The system cannot find the path specified: '
                            f'{error2.filename}')
-
+        sys.exit(1)
     except KeyError as key_error:
         print(str(key_error))
-
+        sys.exit(1)
     except requests.exceptions.ConnectionError:
         logger_error.error(f'Failed to establish a connection to site: '
                            f'{parse_cli_arguments().url}\n'
                            f'Please check your a connection to Ethernet '
                            f'or address site')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
